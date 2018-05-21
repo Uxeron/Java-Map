1ec6294c8581bfaa0a8dccd54f273d8cc4bfe1c4
Uxeron <vgabrielius123@gmail.com>
2018-05-21 18:59:43 +0300

    Initial commit

---
+++ desktop/src/com/uxeron/map/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.uxeron.map.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.uxeron.map.Map;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Map(), config);
+	}
+}
